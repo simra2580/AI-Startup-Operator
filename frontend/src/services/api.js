63c61cfecc105d823e79132cb5b83faacf622000
Simra Begum <simrabegum28@gmail.com>
2026-05-17 18:04:43 +0530

    fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-startup-operator.onrender.com/api",
 });
 
 export default API;
